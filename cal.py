a1442d25b796c2c926e51aa1dcc4cee68c7d133a
jiangph2016 <jiangph1001@qq.com>
2020-02-14 20:53:35 +0800

    2-14 更新数据

--- cal.py
+++ cal.py
@@ -14,6 +14,7 @@ from matplotlib import pyplot as plt
 begin_day = 1
 day_num = 25
 predict_day = 1
+flag = False #用于控制是否按疑似人数进行预测
 
 
 ## 生成包含疑似人数的特征
@@ -48,21 +49,24 @@ def predict_new(clf,data,rown,degree,color,label):
     day_range=np.linspace(begin_day,begin_day + day_num - 1,day_num) # [1,2,...,day_num]
     result = clf.predict(poly_reg.fit_transform(day_range.reshape(-1,1)))
     print("                            ",np.around(result[-1],decimals=1))
-    plt.plot(day_range,result,color=color,linestyle='--',marker='.',label=label)
+    plt.plot(day_range,result,color=color,linestyle='--',marker=',',label=label)
 
 
 ## 进行预测
 def predict(clf,data):
     rown = 2 # 1代表对武汉的确诊人数进行回归，2代表对总确诊人数进行回归,3代表对疑似人数进行回归，以此类推
     plt.figure()
-    predict_new(clf,data,rown,4,'red','4')
+    
+    predict_new(clf,data,rown,4,'green','4')
     predict_new(clf,data,rown,5,'blue','5')
     predict_new(clf,data,rown,6,'fuchsia','6')
     predict_new(clf,data,rown,7,'orange','7')
-    if rown == 2 and predict_day == 1:
+    
+    if rown == 2 and predict_day == 1 and flag == True:
         predict_by_suspect(clf,data,2,4,'cyan','4+suspect')
         predict_by_suspect(clf,data,2,5,'m','5+suspect')
-    plt.plot(np.arange(1,len(data[0])+1),data[rown],color='g',linestyle='--',marker='o',label='true') # 画出真实值
+    #plt.plot(data[0],data[rown],color='red',linestyle=':',marker='.',label='true') # 画出真实值
+    plt.scatter(data[0],data[rown],c='r',marker='o')# 画出真实值
     plt.title("COVID-19")
     plt.legend()
     plt.show()
@@ -70,11 +74,11 @@ def predict(clf,data):
 ## 读取数据
 def read_data():
     global day_num
-    with open("data.csv","r") as f:
-        data = np.loadtxt(f,delimiter = ",",comments='#')
+    with open("NCPdata.csv","r") as f:
+        data = np.loadtxt(f,delimiter = ",",comments='#',skiprows = 1)
         data = data[...,:] # 对数据切片，回溯前几天的预测值，对比准确性时用到
-        day_num = len(data[0])+predict_day
-        print()
+        day_num = data[-1][0] + predict_day
+        data = data.T
         return data
         
 if __name__ == "__main__":
